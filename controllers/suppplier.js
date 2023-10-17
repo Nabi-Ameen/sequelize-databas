27fbafbc4045325c82fc2cefe136c0340ff06a6f
Nabi Ameen <nabiameen35126@gmail.com>
2023-10-17 15:01:31 +0500

    changes

--- controllers/suppplier.js
+++ controllers/suppplier.js
@@ -1,23 +1,19 @@
 const Supplier = require("../models/supplier");
 
 const postSupplier = async (req, res) => {
-  console.log(req.body);
   try {
     const supData = req.body;
-    await Supplier
-      .create(supData)
+    await Supplier.create(supData)
       .then((createdata) => {
         res
           .status(200)
           .send({ success: true, message: "Data Created successfully" });
       })
       .catch((err) => {
-        res
-          .status(400)
-          .send({
-            success: false,
-            message: "something went wrong.please try again!",
-          });
+        res.status(400).send({
+          success: false,
+          message: "something went wrong.please try again!",
+        });
       });
   } catch (error) {
     console.log(error);
@@ -25,4 +21,23 @@ const postSupplier = async (req, res) => {
   }
 };
 
-module.exports = { postSupplier };
+const getSupplier = async (req, res) => {
+  try {
+    const data = await Supplier.findAll()
+    res.status(200).send(data)
+    // .then((getData)=>{
+    //     res.status(200).send(getData)
+    // })
+    // .catch(()=>{
+    //     res.status(400).send({
+    //         success: false,
+    //         message: "something went wrong!"
+    //     })
+    // })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: "internal server error" + error });
+  }
+};
+
+module.exports = { postSupplier, getSupplier };
--- routes/supplier.js
+++ routes/supplier.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const {suppCtrl} = require("../controllers")
 
 
+router.get("/get", async(req, res)=>{
+    return suppCtrl.getSupplier(req, res)
+})
 
 router.post("/post", async(req , res)=>{
     return suppCtrl.postSupplier(req , res)
