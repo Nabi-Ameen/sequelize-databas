61a255eabeb9fc616298cff63bbe999e3fb4b365
nabiameen <nabiameen35126@gmail.com>
2023-11-01 21:41:32 +0500

    changes

--- controllers/supplierRuls.js
+++ controllers/supplierRuls.js
@@ -1,5 +1,5 @@
 const SupplierModel = require("../models/supplierRulsModel");
-const fs = require("fs")
+const fs = require("fs");
 
 const getSupplierRuls = async (req, res) => {
   try {
@@ -16,20 +16,25 @@ const getSupplierRuls = async (req, res) => {
 const createSupplierRules = async (req, res) => {
   try {
     const suppBody = req.body;
-    const validateImage = (req, res, next)=>{
-      if(req.file){
-        const allowedExtention = ["jpg", "png"]
+    const validateImage = (req, res, next) => {
+      if (req.file) {
+        const allowedExtention = ["jpg", "png", "jpeg"];
         const fileExtenion = req.file.mimetype.split("/")[1];
-        if(allowedExtention.includes(fileExtenion)){
-          return next()
-        }else{
-          return res.status(400).json({success: false, message: "Only PNG and JPG images are allowed."})
+        if (allowedExtention.includes(fileExtenion)) {
+          return next();
+        } else {
+          return res
+            .status(400)
+            .json({
+              success: false,
+              message: "Only PNG and JPG images are allowed.",
+            });
         }
       }
       return next();
     };
 
-    validateImage(req, res, async()=>{
+    validateImage(req, res, async () => {
       let uploadimage = null;
       if (req.file) {
         uploadimage = req.file.filename;
@@ -42,9 +47,15 @@ const createSupplierRules = async (req, res) => {
         ...suppBody,
         image: uploadimage,
       });
-  
-      res.status(201).json({ success: true,message: "Supplier rule created successfully", data: createSupplierRule });
-    })
+
+      res
+        .status(201)
+        .json({
+          success: true,
+          message: "Supplier rule created successfully",
+          data: createSupplierRule,
+        });
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal server error" });
@@ -72,7 +83,7 @@ const updateSingleSupplierRule = async (req, res) => {
     const id = req.params.id;
     const body = req.body;
 
-    const oldimageurl = await SupplierModel.findOne({where:{id:id}})
+    const oldimageurl = await SupplierModel.findOne({ where: { id: id } });
     if (req.file) {
       uploadimage = req.file.filename;
     } else {
@@ -80,17 +91,16 @@ const updateSingleSupplierRule = async (req, res) => {
         .status(400)
         .json({ success: false, message: "image is required" });
     }
-    if(oldimageurl && oldimageurl != undefined){
-      const path  = `../uploads/${oldimageurl}`
-      fs.unlink(path,(err)=>{
-        console.log("Error:",err)
-      })
+    if (oldimageurl && oldimageurl != undefined) {
+      const path = `../uploads/${oldimageurl}`;
+      fs.unlink(path, (err) => {
+        console.log("Error:", err);
+      });
     }
     const rowsUpdated = await SupplierModel.update(
-      {...body ,
-      image:uploadimage,},
-      { where: { id: id }
-     });
+      { ...body, image: uploadimage },
+      { where: { id: id } }
+    );
 
     if (rowsUpdated) {
       res
@@ -107,25 +117,27 @@ const updateSingleSupplierRule = async (req, res) => {
   }
 };
 
-const deleteSupplierRule = async(req, res)=>{
+const deleteSupplierRule = async (req, res) => {
   try {
     const id = req.params.id;
-    const supplierRule = await SupplierModel.destroy({where: {id: id}})
-    if(supplierRule){
-      res.status(200).json({success: true, message: "record deleted"})
-    }else{
-      res.status(200).json({success: false, message: "Record not found"})
+    const supplierRule = await SupplierModel.destroy({ where: { id: id } });
+    if (supplierRule) {
+      res.status(200).json({ success: true, message: "record deleted" });
+    } else {
+      res.status(200).json({ success: false, message: "Record not found" });
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json({success: false, message: "internal server error" + error})
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "internal server error" + error });
   }
-}
+};
 
 module.exports = {
   createSupplierRules,
   getSupplierRuls,
   getSingleSupplierRule,
   updateSingleSupplierRule,
-  deleteSupplierRule
+  deleteSupplierRule,
 };
