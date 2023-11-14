b2362b7c959281771065ea0e1349fd9ae197b338
nabiameen <nabiameen35126@gmail.com>
2023-11-14 07:19:22 +0500

    changes

--- controllers/fligtgroup.js
+++
@@ -1,5 +0,0 @@
-const flight = require("../models/flight");
-
-const createflightGroup = async (req, res) => {};
-
-module.exports = { createflightGroup };
--- routes/flight.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/post", async (req, res) => {
-  return fltCtrl.createflightGroup(req, res);
-});
