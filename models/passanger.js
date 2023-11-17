26dc7b50c7464501ed6d08402549cdd07ebfab9d
Nabi Ameen <nabiameen35126@gmail.com>
2023-11-17 16:47:43 +0500

    changes

--- models/passanger.js
+++ models/passanger.js
@@ -1,17 +1,15 @@
 const sequelize = require("../db/db_config");
 const Sequelize = require("sequelize");
 const { DataTypes } = Sequelize;
-const flightGroup = require("./flight_group")
+const flightGroup = require("./flight_group");
 const Passanger = sequelize.define("passanger", {
-  id:
-  {
-    type:DataTypes.INTEGER,
-    autoIncrement:true,
-    primaryKey:true
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  flight_id:
-  {
-    type:DataTypes.INTEGER
+  flight_id: {
+    type: DataTypes.INTEGER,
   },
   passangerType: {
     type: DataTypes.STRING,
@@ -28,6 +26,5 @@ const Passanger = sequelize.define("passanger", {
 });
 flightGroup.hasMany(Passanger, { foreignKey: "flight_id" });
 Passanger.belongsTo(flightGroup, { foreignKey: "flight_id" });
-
 
 module.exports = Passanger;
